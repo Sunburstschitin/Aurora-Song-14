66b1dccf0e4c8a2fd95cb066a1e9f28cfeabcfd5
Falcon <falcon@zigtag.dev>
2026-06-17 21:14:57 -0700

    style: cleaned up RechargeableBlockingComponent

--- Content.Server/_White/Blocking/RechargeableBlockingComponent.cs
+++ Content.Server/_White/Blocking/RechargeableBlockingComponent.cs
@@ -1,18 +1,4 @@
 namespace Content.Server._White.Blocking;
 
 [RegisterComponent]
-public sealed partial class RechargeableBlockingComponent : Component
-{
-    // Aurora's Song | As of right now none of these variables actually define or change any behaviour in the system's
-    // logic, which we may change in the future but this was the easiest way to make this work with the more updated power
-    // and battery systems we've inherited from Upstream(WizDen)
-
-    // [DataField, ViewVariables(VVAccess.ReadWrite)]
-    // public float DischargedRechargeRate = 1.33f;
-    //
-    // [DataField, ViewVariables(VVAccess.ReadWrite)]
-    // public float ChargedRechargeRate = 2f;
-
-    // [ViewVariables] //Aurora's Song | This state has been the bane of my existence, good riddance.
-    // public bool Discharged;
-}
+public sealed partial class RechargeableBlockingComponent : Component { } // Aurora's Song - Remove variables from here and use upstream components instead
